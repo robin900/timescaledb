420829f9091e3616c0698b8bac413eb85d9b1eba
Robin Thomas <rthomas900@gmail.com>
2017-04-29 16:40:02 -0400

    non-working sample of insert test on rowcount

--- test/sql/insert_single.sql
+++ test/sql/insert_single.sql
@@ -6,7 +6,9 @@ SELECT * FROM "one_Partition" ORDER BY "timeCustom", device_id;
 --test that we can insert data into a 1-dimensional table (only time partitioning)
 CREATE TABLE "1dim"(time timestamp, temp float);
 SELECT create_hypertable('"1dim"', 'time');
+\set QUIET off
 INSERT INTO "1dim" VALUES('2017-01-20T09:00:01', 22.5);
 INSERT INTO "1dim" VALUES('2017-01-20T09:00:21', 21.2);
 INSERT INTO "1dim" VALUES('2017-01-20T09:00:47', 25.1);
+\set QUIET on
 SELECT * FROM "1dim";
